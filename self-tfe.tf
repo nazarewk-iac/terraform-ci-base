24ae7e807c8a6d69b09ccdd954b0c8ab4f5a67ad
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-02-20 14:18:10 +0100

    try to add GitHub VCS provider

--- self-tfe.tf
+++ self-tfe.tf
@@ -13,3 +13,11 @@ resource "tfe_workspace" "self" {
     ]
   }
 }
+
+resource "tfe_oauth_client" "self" {
+  organization     = tfe_organization.self.name
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_token
+  service_provider = "github"
+}
