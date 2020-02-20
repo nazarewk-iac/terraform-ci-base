6374c384013d66cfc92c86b60555e37e11810e43
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-02-20 14:22:46 +0100

    add GitLab, state mv GitHub

--- self-tfe.tf
+++ self-tfe.tf
@@ -14,7 +14,15 @@ resource "tfe_workspace" "self" {
   }
 }
 
-resource "tfe_oauth_client" "self" {
+resource "tfe_oauth_client" "self-gitlab" {
+  organization     = tfe_organization.self.name
+  api_url          = "https://gitlab.com/api/v4"
+  http_url         = "https://gitlab.com"
+  oauth_token      = var.gitlab_token
+  service_provider = "gitlab"
+}
+
+resource "tfe_oauth_client" "self-github" {
   organization     = tfe_organization.self.name
   api_url          = "https://api.github.com"
   http_url         = "https://github.com"
