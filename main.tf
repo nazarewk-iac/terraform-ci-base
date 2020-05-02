ac3139e0bb5c358d8e76b8b34314b3bb0ffee99c
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-05-02 23:55:19 +0200

    add branch protections to masters on GitHub

--- main.tf
+++ main.tf
@@ -5,3 +5,21 @@ resource "github_repository" "packer-rpi-k3s" {
 resource "github_repository" "rpi4-k3os" {
   name = "rpi4-k3os"
 }
+
+locals {
+  repositories = [
+    github_repository.rpi4-k3os,
+    github_repository.packer-rpi-k3s,
+    github_repository.self
+  ]
+}
+
+resource "github_branch_protection" "masters" {
+  for_each = {for repo in local.repositories : repo.name => repo}
+
+  repository = each.key
+
+  branch = "master"
+
+  require_signed_commits = true
+}
