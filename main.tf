8289df7654df7bfb0f0c1d34c14be1fdd4a74525
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-02-03 10:31:37 +0100

    un-archive

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "github_repository" "terraform-provider-custom" {
   visibility             = "public"
   auto_init              = true
   delete_branch_on_merge = true
-  archived               = true
+  archived               = false
 }
 
 resource "github_repository" "archpi" {
